9ecde988edafded8c6349ddd4e899653269ce6fe
Aleksi Muikku <aleksi.muikku@hotmail.com>
2020-09-30 13:56:45 +0300

    Add functionality to ask for user input in Main.java.

--- src/muikku.app/Main.java
+++ src/muikku.app/Main.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
-    System.out.println(MathUtils.random(0, 3));
+    Scanner input = new Scanner(System.in);
+    int min = 0;
+    System.out.println("Welcome to randomizer! \n Please give a number: ");
+    int max = Integer.parseInt(input.nextLine());
+    System.out.println("Here's a number between 0 and " + max + "!: "
+       + MathUtils.random(min, max));
   }
 }
