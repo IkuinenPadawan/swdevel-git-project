6e85567444cd7ff8cc7c6652de6a6eb3208ee2f5
Juuso Melentjeff <juuso.melentjeff@gmail.com>
2020-09-30 14:12:25 +0300

    Add javadoc to the sum method of Math class.

--- src/Math.java
+++ src/Math.java
@@ -1,4 +1,12 @@
 Public class Math {
+
+    /**
+    * Returns the sum of two integers.
+    *
+    * @param a integer to be summed
+    * @param b integer to be summed
+    * @return sum of a and b
+    */
     static void sum(int a, int b) {
         return a + b;
     }
