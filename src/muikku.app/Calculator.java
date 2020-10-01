01df0ab2c13fd0e78e02ed5c4696d15f01270889
Aleksi Muikku <aleksi.muikku@hotmail.com>
2020-10-01 11:01:48 +0300

    Add functionality for user input in Calculator.java
    A very simple user input query to do additions with.

--- src/muikku.app/Calculator.java
+++ src/muikku.app/Calculator.java
@@ -1,7 +1,14 @@
+import java.util.Scanner;
+
 class Calculator {
   public static void main (String[] args) {
-    System.out.println(add(5, 5));
+    Scanner input = new Scanner(System.in);
+    System.out.println("Welcome to Calculator! What would you like to calculate?");
+    int a = Integer.parseInt(input.nextLine());
+    int b = Integer.parseInt(input.nextLine());
+    System.out.println("result: " + add(a, b)); 
   }
+
   static int add(int a, int  b) {
     return a + b;
   }
