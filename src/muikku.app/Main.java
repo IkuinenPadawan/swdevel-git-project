b69323c0cf9dfdab67f0447ecbcf64217e72f31c
Aleksi Muikku <aleksi.muikku@hotmail.com>
2020-10-02 12:54:10 +0300

    Remove Scanner import and references from Main.java.

--- src/muikku.app/Main.java
+++ src/muikku.app/Main.java
@@ -1,12 +1,9 @@
-import java.util.Scanner;
-
 class Main {
   public static void main(String[] args) {
-    Scanner input = new Scanner(System.in);
     int min = 0;
     System.out.println("Welcome to randomizer! \n Please give a number: ");
     int max = Util.getInputInteger();
-    System.out.println("Here's a number between 0 and " + max
+    System.out.println("Here's a number between 0 and " + max + ": "
        + MathUtils.random(min, max));
   }
 }
