76a18785512f94611fc90426796359065125a48a
Aleksi Muikku <aleksi.muikku@hotmail.com>
2020-10-02 12:48:06 +0300

    Modify Main.java to use new Util class.
    Main.java now calls Util.getInputInteger() to get user input.

--- src/muikku.app/Main.java
+++ src/muikku.app/Main.java
@@ -5,8 +5,8 @@ class Main {
     Scanner input = new Scanner(System.in);
     int min = 0;
     System.out.println("Welcome to randomizer! \n Please give a number: ");
-    int max = Integer.parseInt(input.nextLine());
-    System.out.println("Here's a number between 0 and " + max + "!: "
+    int max = Util.getInputInteger();
+    System.out.println("Here's a number between 0 and " + max
        + MathUtils.random(min, max));
   }
 }
