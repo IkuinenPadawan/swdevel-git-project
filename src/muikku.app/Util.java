66fead3870b9e6dfc791a80f06c577d900b9bda9
Aleksi Muikku <aleksi.muikku@hotmail.com>
2020-10-02 12:46:00 +0300

    Add class Util. Contains method getInputInteger().
    Method getInputInteger() asks for user input and returns it as a string.

---
+++ src/muikku.app/Util.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+class Util {
+  static Scanner scanner = new Scanner(System.in);
+
+  public static int getInputInteger() {
+    return Integer.parseInt(scanner.nextLine());
+  }
+}
