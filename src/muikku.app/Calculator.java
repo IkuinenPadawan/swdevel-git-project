5df8fd5e680684a8a9247834c95e53038109de8b
Aleksi Muikku <aleksi.muikku@hotmail.com>
2020-10-05 11:44:05 +0300

    Remove Scanner import & references from Calculator.java.
    Util class is used for input from now on.

--- src/muikku.app/Calculator.java
+++ src/muikku.app/Calculator.java
@@ -1,11 +1,7 @@
-import java.util.Scanner;
-
 class Calculator {
   public static void main (String[] args) {
-    Scanner input = new Scanner(System.in);
     System.out.println("Welcome to Calculator! What would you like to calculate?");
-    int a = Integer.parseInt(input.nextLine());
-    int b = Integer.parseInt(input.nextLine());
-    System.out.println("result: " + MathUtils.add(a, b)); 
+    String input = Util.getInputString();
+    System.out.println("result: " + input); 
   }
 }
