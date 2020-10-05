4ea3f9ccc8ed0ee075bc84719efa5780d815d34a
Aleksi Muikku <aleksi.muikku@hotmail.com>
2020-10-05 12:20:35 +0300

    Add method parseInput(String s) to Calculator.java
    Method parseInput(String s) goes through an input string and returns
    an array with the seperated elements. Example:
    Input: "100 - 50"
    Array = { "100", "-", "50" }

--- src/muikku.app/Calculator.java
+++ src/muikku.app/Calculator.java
@@ -2,6 +2,20 @@ class Calculator {
   public static void main (String[] args) {
     System.out.println("Welcome to Calculator! What would you like to calculate?");
     String input = Util.getInputString();
-    System.out.println("result: " + input); 
+    System.out.println("result: " + input);
+  }
+
+  private static String[] parseInput(String s) {
+    String[] arr = {"", "", ""};
+    int index = 0;
+
+    for (int i = 0; i < s.length(); i++) {
+      if (s.charAt(i) == ' ') {
+        index++;
+      } else {
+        arr[index] += s.charAt(i);
+      }
+    }
+    return arr;
   }
 }
