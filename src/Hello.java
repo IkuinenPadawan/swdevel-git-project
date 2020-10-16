c84e6f08a9da778dd3c825be96eed63a91f71433
Juuso Melentjeff <juuso.melentjeff@gmail.com>
2020-10-16 11:09:11 +0300

    Prints uselessMethodFour method. Prints its purpose.

--- src/Hello.java
+++ src/Hello.java
@@ -96,4 +96,8 @@ Public class Hello {
     public static void uselessMethodThree() {
         print("This is a useless method no 3");
     }
+
+    public static void uselessMethodFour() {
+        print("This is a useless method no 4");
+    }
 }
