b0c2254cf92de85ac336c776c830d05e4251614d
Aleksi Muikku <aleksi.muikku@hotmail.com>
2020-10-20 23:49:51 +0300

    Make Calculator runnable from Main.java. Feature to be expanded.

--- src/muikku.app/Main.java
+++ src/muikku.app/Main.java
@@ -1,9 +1,7 @@
 class Main {
   public static void main(String[] args) {
-    int min = 0;
-    System.out.println("Welcome to randomizer! \n Please give a number: ");
-    int max = Util.getInputInteger();
-    System.out.println("Here's a number between 0 and " + max + ": "
-       + MathUtils.random(min, max));
+    System.out.println("Yo welcome to this magnificent app! ");
+    Calculator.main(args);
+    System.out.println("Bye! ");
   }
 }
