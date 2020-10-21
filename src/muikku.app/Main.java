6b5762e60a417fb4347d3253daa6bd97a4c15b0a
Aleksi Muikku <aleksi.muikku@hotmail.com>
2020-10-21 23:30:55 +0300

    Add Main::appChooser(String, String[]), and call it. User can choose app to run
    Takes user input to decide which app to run.
    Passes commandline arguments along to the apps.

--- src/muikku.app/Main.java
+++ src/muikku.app/Main.java
@@ -1,7 +1,21 @@
 class Main {
   public static void main(String[] args) {
     System.out.println("Yo welcome to this magnificent app! ");
-    Calculator.main(args);
+    String input = Util.getInputString();
+    appChooser(input, args);
     System.out.println("Bye! ");
+  }
+
+  private static void appChooser(String input, String[] args) {
+    switch (input) {
+      case "rng":
+        Rng.main(args);
+        break;
+      case "calc":
+        Calculator.main(args);
+        break;
+      default:
+        System.out.println("try again.");
+    }
   }
 }
