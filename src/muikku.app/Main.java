5ddda2876b36ebd5a7f0ff2f0823012d304c5c3e
Aleksi Muikku <aleksi.muikku@hotmail.com>
2020-10-21 23:58:08 +0300

    Add a print to tell user about their options when running Main.

--- src/muikku.app/Main.java
+++ src/muikku.app/Main.java
@@ -1,6 +1,7 @@
 class Main {
   public static void main(String[] args) {
     System.out.println("Yo welcome to this magnificent app! ");
+    System.out.println("Type 'calc' or 'rng' to run an app:");
     String input = Util.getInputString();
     appChooser(input, args);
     System.out.println("Bye! ");
