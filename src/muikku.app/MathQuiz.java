5004d7b1eb299a9ead687a800269122332759241
Aleksi Muikku <aleksi.muikku@hotmail.com>
2020-10-22 19:01:31 +0300

    Add quick first implementation of MathQuiz
    MathQuiz asks the user for an answer to a calculation
    which the user then answers. User gets feedback if it
    was correct or not. Really simple implementation for now.

--- src/muikku.app/MathQuiz.java
+++ src/muikku.app/MathQuiz.java
@@ -1,4 +1,20 @@
 class MathQuiz {
   public static void main(String[] args) {
-    
+    int answer = generateQuestion();
+    int input = Util.getInputInteger();
+    System.out.println(answer == input);
   }
+
+  private static int generateQuestion() {
+    // either no divisions or just round everything to int
+    // randomize operator & operands,
+    // display as calculation
+    // calculate result
+    // return result to compare against
+
+    int a = MathUtils.random(1, 10);
+    int b = MathUtils.random(1, 10);
+    System.out.println("What is the result of " + a + " + " + b + "?");
+    return a + b;
+  } 
+}
