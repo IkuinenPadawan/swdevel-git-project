f27e8edc72138dca440e6b945ccbecd2f41cf6e3
Aleksi Muikku <aleksi.muikku@hotmail.com>
2020-11-02 18:33:51 +0200

    Add method getRawInput() & Scanner import to Utils.java.

--- src/Game/Utils.java
+++ src/Game/Utils.java
@@ -1,3 +1,13 @@
+import java.util.Scanner;
+
 public class Utils {
-  
+  /**
+   * Method to get user input from command line.
+   * 
+   * @return user input as a string
+   */
+  public static String getRawInput() {
+    Scanner scanner = new Scanner(System.in);
+    return scanner.nextLine();
+  }
 }
