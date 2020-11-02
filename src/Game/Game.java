21d71fab7ee2f57e2ff9a1595a0fee14f29b0c7b
Juuso Melentjeff <juuso.melentjeff@gmail.com>
2020-11-02 16:16:58 +0200

    Initial commit of Game.java class.

--- src/Game/Game.java
+++ src/Game/Game.java
@@ -1,3 +1,15 @@
 public class Game {
       
+    public Game() {
+    
+    }
+
+    public void start() {
+        startingRoom();
+    }
+
+    public void startingRoom() {
+        System.out.println("You are in a dark room.")
+    }
+
 }
