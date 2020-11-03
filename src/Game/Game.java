610acc4c2c7c5c8b534be4d1c7b65de233f29330
Juuso Melentjeff <juuso.melentjeff@gmail.com>
2020-11-03 11:02:12 +0200

    Add first case of opening room.

--- src/Game/Game.java
+++ src/Game/Game.java
@@ -19,7 +19,7 @@ public class Game {
     }
 
     private void startingRoom() {
-        System.out.println("You wake up in a dark room feeling groggy. What do you do?");
+        System.out.println("You wake up in a dark room feeling groggy and utterly alone. What do you do?");
         System.out.println("  1. Carefully feel yourself around the room to get clues of where you are.");
         System.out.println("  2. Open your eyes");
 
@@ -27,6 +27,9 @@ public class Game {
 
         switch(action) {
           case 1:
+            System.out.println("You fumble about the room. You almost trip on something that makes a metallic noise when you stumble upon it. Air feels chilly. There's a draft. You reach out carefully and finally make contact with a wall of sorts. You feel around. The wall seems to be made out of slabs of stone. Your immediate thought of your whereabouts is a medieval dungeon. Cold chill rund through your body. What the hell has happened? You trace along the wall and reach an opening.");
+            System.out.println("  1. Move through the opening.");
+            System.out.println("  2. Open your eyes");
             break;
 
           case 2:
