464bb08c974435a2da51e100c124281f8ca98bf8
Juuso Melentjeff <juuso.melentjeff@gmail.com>
2020-11-03 12:20:50 +0200

    Fix typo on introduction case 2. Missing 's'.

--- src/Game/Game.java
+++ src/Game/Game.java
@@ -36,7 +36,7 @@ public class Game {
           case 2:
               System.out.println("Brightest lights you have ever seen sear through your retina. You close your eyes immediately in utter pain while falling to your knees. Hard stone floor almost shatters your knees and you cry out in mindnumbing pain. Pain induced tears start to form up in your eyes, increasing the burning feeling. The pain is almost intolerable.");
               System.out.println("  1. You remember that pee helps to reduce burning pain. Contort yourself to a position where you can pee in your eyes.");
-              ystem.out.println("  2. Crawl forward in search of a light switch");
+              System.out.println("  2. Crawl forward in search of a light switch");
               int action = Integer.parseInt(Utils.getRawInput());
             break;
         }
