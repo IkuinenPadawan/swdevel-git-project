6f53c96eb88fa1b0b79befad7ade9df98987ef94
Juuso Melentjeff <juuso.melentjeff@gmail.com>
2020-11-03 11:53:12 +0200

    Add case 2 to startingRoom.

--- src/Game/Game.java
+++ src/Game/Game.java
@@ -27,12 +27,15 @@ public class Game {
 
         switch(action) {
           case 1:
-            System.out.println("You fumble about the room. You almost trip on something that makes a metallic noise when you stumble upon it. Air feels chilly. There's a draft. You reach out carefully and finally make contact with a wall of sorts. You feel around. The wall seems to be made out of slabs of stone. Your immediate thought of your whereabouts is a medieval dungeon. Cold chill rund through your body. What the hell has happened? You trace along the wall and reach an opening.");
-            System.out.println("  1. Move through the opening.");
-            System.out.println("  2. Open your eyes");
+              System.out.println("You fumble about the room. You almost trip on something that makes a metallic noise when you stumble upon it. Air feels chilly. There's a draft. You reach out carefully and finally make contact with a wall of sorts. You feel around. The wall seems to be made out of slabs of stone. Your immediate thought of your whereabouts is a medieval dungeon. Cold chill rund through your body. What the hell has happened? You trace along the wall and reach an opening.");
+              System.out.println("  1. Move through the opening.");
+              System.out.println("  2. Open your eyes");
             break;
 
           case 2:
+              System.out.println("Brightest lights you have ever seen sear through your retina. You close your eyes immediately in utter pain while falling to your knees. Hard stone floor almost shatters your knees and you cry out in mindnumbing pain. Pain induced tears start to form up in your eyes, increasing the burning feeling. The pain is almost intolerable.");
+              System.out.println("  1. You remember that pee helps to reduce burning pain. Contort yourself to a position where you can pee in your eyes.");
+              ystem.out.println("  2. Crawl forward in search of a light switch");
             break;
         }
 
