ecc18bd70488e3a071162e8625dd560dfe6b2f1a
Juuso Melentjeff <juuso.melentjeff@gmail.com>
2020-11-03 12:16:37 +0200

    Add text to Game.java introduction.

--- src/Game/Game.java
+++ src/Game/Game.java
@@ -15,7 +15,7 @@ public class Game {
       System.out.println("Welcome to " + gameName + "!");
       System.out.println("What would you like to be called?: ");
       playerName = Utils.getRawInput();
-      System.out.println("Have fun " + playerName + "!" + "\n\n\n\n");
+      System.out.println("What kind of name is that? Well, anyway, have fun " + playerName + "!" + "\n\n\n\n");
     }
 
     private void startingRoom() {
