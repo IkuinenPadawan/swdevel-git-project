d21f7ccd5edb4b4d97e275df9b1781c787b38b8c
Aleksi Muikku <aleksi.muikku@hotmail.com>
2020-11-05 00:17:39 +0200

    Remove duplicate declarations of type for int action in Game.startingRoom().

--- src/Game/Game.java
+++ src/Game/Game.java
@@ -36,7 +36,7 @@ public class Game {
               System.out.println("You fumble about the room. You almost trip on something that makes a metallic noise when you stumble upon it. Air feels chilly. There's a draft. You reach out carefully and finally make contact with a wall of sorts. You feel around. The wall seems to be made out of slabs of stone. Your immediate thought of your whereabouts is a medieval dungeon. Cold chill rund through your body. What the hell has happened? You trace along the wall and reach an opening.");
               System.out.println("  1. Move through the opening.");
               System.out.println("  2. Open your eyes");
-              int action = Integer.parseInt(Utils.getRawInput());
+              action = Integer.parseInt(Utils.getRawInput());
               if (action == 1) {
                 roomOne(1);
               }
@@ -46,7 +46,7 @@ public class Game {
               System.out.println("Brightest lights you have ever seen sear through your retina. You close your eyes immediately in utter pain while falling to your knees. Hard stone floor almost shatters your knees and you cry out in mindnumbing pain. Pain induced tears start to form up in your eyes, increasing the burning feeling. The pain is almost intolerable.");
               System.out.println("  1. You remember that pee helps to reduce burning pain. Contort yourself to a position where you can pee in your eyes.");
               System.out.println("  2. Crawl forward in search of a light switch");
-              int action = Integer.parseInt(Utils.getRawInput());
+              action = Integer.parseInt(Utils.getRawInput());
             break;
         }
 
